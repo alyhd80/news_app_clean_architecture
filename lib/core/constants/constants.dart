e77649d7bea459d06dfd0a99e39dd940d28ad4bf
Ali Heidari <a.heidari@cpol.co>
2025-03-18 00:46:51 +0330

    first commit

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+
+const String newsApiBaseURL='https://newsapi.org/v2';
+const String newsAPIKey='18e99343a3894885952d43b038d9fe14';
+const String countryQuery='us';
+const String categoryQuery='general';
+
